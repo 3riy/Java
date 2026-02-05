4f6d7251a84eae9e7bd39095565bc6033e83af61
Riya halbhavi <halbhaviriya@gmail.com>
2026-02-05 20:52:28 +0530

    Add files via upload

---
+++ Conditional Statements/while/Count_Digits.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Count_Digits {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int no = scanner.nextInt();
+
+        int count = 0;
+
+        while (no != 0) {
+            no = no / 10;
+            count++;
+        }
+        System.out.println("Number of digits = " + count);
+
+        scanner.close();
+    }
+}
---
+++ Conditional Statements/while/Print_Nos_1_to_10.java
@@ -0,0 +1,10 @@
+class Print_Nos_1_to_10 {
+    public static void main(String[] args) {
+        int i = 1;
+6
+        while (i<=10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
